519f664b659db51b18d3e63b22271485ebe03102
Ryan Homer <ryan@murage.ca>
2016-10-14 15:36:04 -0700

    Crash fixes.

--- android/src/main/java/com/oney/gcm/BackgroundService.java
+++ android/src/main/java/com/oney/gcm/BackgroundService.java
@@ -44,8 +44,8 @@ public class BackgroundService extends Service {
     public void onDestroy() {
         Log.d(TAG, "onDestroy");
         super.onDestroy();
-        mReactInstanceManager.onPause();
-        mReactInstanceManager.onDestroy();
+//         mReactInstanceManager.onPause();
+//         mReactInstanceManager.onDestroy();
         mReactInstanceManager = null;
     }
 
